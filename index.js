54f95d61028a6c75e880c0c106cc35790b1cc91b
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2020-03-09 18:15:19 -0400

    Adds basic express setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+// creates a new express application
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+// dynamically set the port to the preset port, or use port 5000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
