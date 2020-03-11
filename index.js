93cd4d1e0e2f6189e1a2e19940c12f290cdb3ae7
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2020-03-11 18:19:00 -0400

    Adds basic passport setup with google-oauth20 strategy

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 // creates a new express application
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there, I've changed" });
-});
+// allows passport to use goggle oauth20 strategy/authentication
+// param (clientID, ClientSecret), provided by oauth service
+passport.use(new GoogleStrategy());
 
 // dynamically set the port to the preset port, or use port 5000
 const PORT = process.env.PORT || 5000;
