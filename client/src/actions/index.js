64992b7471bbd120457c0ad192d5a87deafab7da
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2020-04-03 18:33:26 -0400

    created custom express middleware

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
-import { restart } from "nodemon";
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get("api/current_user");
